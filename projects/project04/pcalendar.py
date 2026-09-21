313104c7ec963f080331c81d295b77c54fdea2e7
Phil Kirlin <kirlinp@rhodes.edu>
2026-09-21 14:27:19 -0500

    proj4

---
+++ projects/project04/pcalendar.py
@@ -0,0 +1,10 @@
+# Author: FILL IN
+# Class: COMP 141
+# Project: Perpetual Calendar
+# Pledge: FILL IN
+# Description: FILL IN
+
+# Reminder: The server will auto-save this file, but only every two minutes or so.
+# That means that whenever you make changes to this file and want to immediately run
+# it in the notebook, you should manually save it here (Ctrl-S on Windows or Command-S on Mac).
+
